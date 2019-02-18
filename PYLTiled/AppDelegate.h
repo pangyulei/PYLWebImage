03272980e0091c0406fd32e12f6efc562a6c67db
yulei pang <yuleipang@yuleidemacbook-pro.local>
2019-02-18 08:07:54 +0800

    Initial Commit

---
+++ PYLTiled/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PYLTiled
+//
+//  Created by yulei pang on 2019/2/18.
+//  Copyright © 2019 pangyulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PYLTiled/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PYLTiled
+//
+//  Created by yulei pang on 2019/2/18.
+//  Copyright © 2019 pangyulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
